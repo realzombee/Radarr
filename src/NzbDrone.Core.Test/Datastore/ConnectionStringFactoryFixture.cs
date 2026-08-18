d393de2403afe35d81e423ec32a22e5ba3011a6b
johoja12 <223961+johoja12@users.noreply.github.com>
2026-08-18 08:57:19 +0000

    fix: wait for transient sqlite locks

---
+++ src/NzbDrone.Core.Test/Datastore/ConnectionStringFactoryFixture.cs
@@ -0,0 +1,21 @@
+using System.Data.SQLite;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Datastore
+{
+    [TestFixture]
+    public class ConnectionStringFactoryFixture : DbTest
+    {
+        [Test]
+        public void should_configure_sqlite_busy_timeout_for_one_second()
+        {
+            var connection = Mocker.Resolve<IConnectionStringFactory>().MainDbConnection.ConnectionString;
+            var builder = new SQLiteConnectionStringBuilder(connection);
+
+            builder.BusyTimeout.Should().Be(1000);
+        }
+    }
+}
